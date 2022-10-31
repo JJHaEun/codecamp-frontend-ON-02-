5d7683f90a6338c020aad880020b5b02dc33eb81
JJHaEun <yj03057@gmail.com>
2022-11-01 00:27:12 +0900

    class_quiz 1 일차

---
+++ class_quiz/pages/01/index.js
@@ -0,0 +1,97 @@
+import {
+  Div,
+  Div2,
+  Div3,
+  Title,
+  Img,
+  Img2,
+  MainDiv,
+  Img3,
+  Div4,
+  Div5,
+  Img4,
+  Span,
+  Div6,
+  Div7,
+  Img5,
+  SPan,
+  Div8,
+} from "../../styles/session01";
+export default function session01() {
+  // 자바스크립트
+  return (
+    <MainDiv>
+      <Div3>
+        <Img3 src="/search.png"></Img3>
+      </Div3>
+      <Div>
+        <Title>마이</Title>
+        <Img src="/img-60-profile-image.png"></Img>
+        <Div2>임정아</Div2>
+        <Img2 src="/Vector.png"></Img2>
+      </Div>
+      <Div4>
+        <span>공지사항</span>
+        <span>이벤트</span>
+        <span>FAQ</span>
+        <span>Q&A</span>
+      </Div4>
+      <Div5>
+        <Span>Q. 01</Span> <div>리뷰 작성은 어떻게 하나요?</div>
+        <div>
+          <Img4 src="/Vector_under.png"></Img4>
+        </div>
+      </Div5>
+      <Div6>
+        <Span>Q. 02</Span> <div>리뷰 수정/삭제는 어떻게 하나요?</div>
+        <div>
+          <Img4 src="/Vector_under.png"></Img4>
+        </div>
+      </Div6>
+      <Div6>
+        <Span>Q. 03</Span> <div>아이디/비밀번호를 잊어버렸어요</div>
+        <div>
+          <Img4 src="/Vector_under.png"></Img4>
+        </div>
+      </Div6>
+      <Div6>
+        <Span>Q. 04</Span> <div>회원탈퇴를 하고싶어요.</div>
+        <div>
+          <Img4 src="/Vector_under.png"></Img4>
+        </div>
+      </Div6>
+      <Div6>
+        <Span>Q. 05</Span> <div>출발지 설정은 어떻게 하나요?</div>
+        <div>
+          <Img4 src="/Vector_under.png"></Img4>
+        </div>
+      </Div6>
+      <Div6>
+        <Span>Q. 06</Span> <div>비밀번호를 변경하고 싶어요</div>
+        <div>
+          <Img4 src="/Vector_under.png"></Img4>
+        </div>
+      </Div6>
+      <Div7>
+        <div>
+          <Img5 src="/home.png"></Img5>
+        </div>
+        <div>
+          <Img5 src="/loc.png"></Img5>
+        </div>
+        <div>
+          <Img5 src="like.png"></Img5>
+        </div>
+        <div>
+          <Img5 src="my.png"></Img5>
+        </div>
+      </Div7>
+      <Div8>
+        <SPan>홈</SPan>
+        <SPan>잇츠로드</SPan>
+        <SPan>마이찜</SPan>
+        <SPan>마이</SPan>
+      </Div8>
+    </MainDiv>
+  );
+}
---
+++ class_quiz/styles/session01.js
@@ -0,0 +1,93 @@
+import styled from "@emotion/styled";
+
+export const Div = styled.div`
+  margin-left: 50px;
+  display: flex;
+  justify-content: flex-end;
+`;
+export const Title = styled.h2`
+  margin-left: 5px;
+`;
+
+export const Img = styled.img`
+  margin-left: 150px;
+  width: 40px;
+  height: 40px;
+  margin-top: 20px;
+`;
+export const Img2 = styled.img`
+  width: 12px;
+  height: 12px;
+  margin-left: 50px;
+  margin-top: 25px;
+  margin-right: 25px;
+`;
+
+export const Div2 = styled.div`
+  font-size: 20px;
+  margin-top: 20px;
+  margin-left: 10px;
+  font-weight: 700;
+`;
+export const Div3 = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 30px;
+`;
+export const Img3 = styled.img`
+  width: 15px;
+  height: 15px;
+  margin: 20px 5px;
+`;
+export const MainDiv = styled.div`
+  width: 640px;
+  height: 1138px;
+`;
+export const Div4 = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding-top: 10px;
+  color: #cacaca;
+  padding-bottom: 25px;
+  border-bottom: 1px solid #cacaca;
+`;
+
+export const Div5 = styled.div`
+  margin-left: 20px;
+  margin-top: 20px;
+`;
+export const Span = styled.span`
+  color: #cacaca;
+  font-size: 10px;
+`;
+export const Img4 = styled.img`
+  width: 20px;
+  height: 10px;
+  margin-left: 550px;
+  margin-top: 0px;
+`;
+export const Div6 = styled.div`
+  margin-left: 20px;
+  margin-top: 50px;
+`;
+export const Div7 = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  border-top: 1px solid #cacaca;
+  margin-top: 270px;
+  padding-top: 5px;
+`;
+export const Img5 = styled.img`
+  width: 30px;
+  height: 27px;
+`;
+export const SPan = styled.span`
+  margin: 0;
+`;
+export const Div8 = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin-right: 10px;
+  margin-left: 15px;
+  font-size: 13px;
+`;
