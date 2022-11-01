7384df0355d04b89c270980dd3309b34d1defd5b
JJHaEun <yj03057@gmail.com>
2022-11-01 10:17:49 +0900

    오늘의 알고리즘 한문제 모름

--- algorihtm/exemple.js
+++ algorihtm/exemple.js
@@ -1,2 +1,49 @@
 const limit = 100;
 const answer = Math.floor(limit / 2);
+
+// 003. 배열의 선언과 할당
+const fruits = ["사과", "바나나", "파인애플"];
+// 004. 배열의 기능
+const newFruits = [];
+
+newFruit.push(fruits - 1);
+
+//배열의 길이가 길면 인덱스를 구하기 어렵기에 인덱스는 0부터시작,
+//길이는 1부터 시작하므로 전체길이에서 1을 뺀인덱스를 구하면
+//마지막 요소가 됨
+// 005. 배열의 기능
+let students = ["철수", "영희", "훈이", "짱구", "유리"];
+
+let newStu = [];
+newStu = students.slice(0, 2);
+
+// 006. 배열의 기능
+
+//let fruits = ["사과", "바나나"]
+//undefined
+//'맛있는 '+fruits[0]
+//'맛있는 사과'
+//for(let i = 0;i<fruits.length;i++){
+//  '맛있는 '+fruits[i]
+
+//}
+//'맛있는 바나나'
+//console.log(fruits)
+//VM414:1 (2) ['사과', '바나나']
+
+// 008. 객체의 선언과 할당
+
+const student = {};
+student.name = "철수";
+
+// 009. 객체의 키&값 추가
+const student = {
+  name: "철수",
+  age: 8,
+};
+
+const school = {
+  name: "다람쥐초등학교",
+  teacher: "다람이",
+};
+student.school = school;
