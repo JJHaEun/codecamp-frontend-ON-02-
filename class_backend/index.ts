e92bd548d0bf3aa7d7ba706a9978b1c2cb0610cf
JJHaEun <yj03057@gmail.com>
2022-11-22 13:04:27 +0900

    중간저장2

--- class_backend/index.ts
+++ class_backend/index.ts
@@ -3,3 +3,16 @@ console.log("반갑습니다");
 const aaa: number = 2;
 
 import { DataSource } from "typeorm";
+import { Board } from "./Board.postgres";
+
+new DataSource({
+  type: "postgres",
+  host: "34.64.114.80",
+  port: 5052,
+  username: "postgres",
+  password: "postgres2022",
+  database: "postgres",
+  synchronize: true,
+  logging: true,
+  entities: [Board],
+});
