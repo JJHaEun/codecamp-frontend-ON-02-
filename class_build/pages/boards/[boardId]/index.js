3b252902cc6311f987030d34b82a2ea0198e886d
JJHaEun <yj03057@gmail.com>
2022-12-22 23:35:03 +0900

    도커 설정해보기 . class_build폴더로 실행까지 실습

--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -9,3 +9,8 @@ export default function BoardsPage() {
     </div>
   );
 }
+
+export const getServerSideProps = () => {
+  // 만약 서버사이드 랜더링을 한다면?? =>out 폴더로 생성 불가.즉 자예 build안됨 => next.config.js에서 exportPathMap으로
+  //  서버사이드 랜더링되는 현제페이지 제외시키기
+};
